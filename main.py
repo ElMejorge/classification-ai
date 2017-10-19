04ce824d5331358a0274faca726cfbd171258f0a
El Mejorge <jorge.montiel@cetys.edu.mx>
2017-10-19 03:16:03 -0700

    get Mean

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import scipy
 import random
 import csv
+import math
 from pandas import DataFrame, Series
 
 #  Obtain dataset as array
@@ -25,7 +26,25 @@ def splitDataset(dataset):
         trainDS.append(testDS.pop(index))
     return [trainDS, testDS]
 
+def separateByClass(dataset):
+    classes = {}
+    for i in range(len(dataset)):
+        element = dataset[i]
+        if (element[-1] not in classes): #Get last attribute in element (which is the class variable (If subject has diabetes or not)
+            classes[element[-1]] = []
+        classes[element[-1]].append(element)
+    return classes
+
+def getMean(dataset):
+    return float(sum(dataset)/len(dataset))
+
+def getStdDev(dataset):
+    return None
+
 # 1-. Handle data
 dataset = loadCsv()
 trainDS, testDS = splitDataset(dataset)
 # 2-. Summarize data
+classes = separateByClass(dataset)
+
+
