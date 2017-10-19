3a6d63d3de8f2bb9fcc5f11807cff86f4da96e23
El Mejorge <jorge.montiel@cetys.edu.mx>
2017-10-19 02:43:03 -0700

    Part 1 done

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import pandas as pd
 import scipy
+import random
 import csv
 from pandas import DataFrame, Series
 
-# Handle data
+#  Obtain dataset as array
 def loadCsv():
-    lines = csv.reader(open("diabetes.data.csv", "rt"))
+    filename = "diabetes.data.csv"
+    lines = csv.reader(open(filename,"rt"))
     dataset = list(lines)
     for i in range(len(dataset)):
         dataset[i] = [float(x) for x in dataset[i]]
-    print('Loaded data file {0} with {1} rows'.format("diabetes", len(dataset)))
     return dataset
 
-loadCsv()
+# Divide dataset into train dataset and test data set.
+# 2/3 an 1/3 is a common ratio for training and testing datasets
+def splitDataset(dataset):
+    ratio = 0.67
+    trainDSLength = int(ratio*len(dataset))
+    trainDS = []
+    testDS = list(dataset)
+    while len(trainDS) < trainDSLength:
+        index = random.randrange(len(testDS))
+        trainDS.append(testDS.pop(index))
+    return [trainDS, testDS]
+
+# 1-. Handle data
+dataset = loadCsv()
+trainDS, testDS = splitDataset(dataset)
+# 2-. Summarize data
